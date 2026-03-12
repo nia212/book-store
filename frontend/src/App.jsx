d1287ecb3599cc8accac621d9cfaf570cb903a83
Nia Nur H <nianurrohmah@students.unnes.ac.id>
2026-03-12 11:50:07 +0700

    small fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,8 +12,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-
-    const timer = setTimeout(() => {
+      const timer = setTimeout(() => {
       setLoading(false);
     }, 2000); 
 
